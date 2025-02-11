72ea1cc7a8cf1fb5955f51c9df87664d89105e77
ArshiLamba <Arshilamba04@gmail.com>
2025-02-11 16:06:53 +0800

    feat: add ReCaptcha API Response

---
+++ Models/ReCaptchaResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ApplicationSecurityApp.Models
+{
+    public class ReCaptchaResponse
+    {
+        public bool success { get; set; }
+        public double score { get; set; }
+    }
+}
